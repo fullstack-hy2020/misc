7740336fcd57bd7152f8fa61b194e3c879f4a9ef
Matti Luukkainen <mluukkai@iki.fi>
2020-02-10 18:36:45 +0200

    Update router-app-v2.js

--- router-app-v2.js
+++ router-app-v2.js
@@ -1,1 +1,163 @@
-asd
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+  useRouteMatch,
+  useHistory,
+} from "react-router-dom"
+
+const Home = () => (
+  <div> 
+    <h2>TKTL notes app</h2> 
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p> 
+  </div>
+)
+
+
+const Note = ({ note }) => {
+  return (
+    <div>
+      <h2>{note.content}</h2>
+      <div>{note.user}</div>
+      <div><strong>{note.important ? 'tärkeä' : ''}</strong></div>
+    </div>
+  )
+}
+
+const Notes = (props) => (
+  <div>
+    <h2>Notes</h2>
+    <ul>
+      {props.notes.map(note =>
+        <li key={note.id}>
+          <Link to={`/notes/${note.id}`}>{note.content}</Link>
+        </li>
+      )}
+    </ul>
+  </div>
+)
+
+const Users = () => (
+  <div>
+    <h2>TKTL notes app</h2>
+    <ul>
+      <li>Matti Luukkainen</li>
+      <li>Juha Tauriainen</li>
+      <li>Arto Hellas</li>
+    </ul>
+  </div>
+)
+
+const Login = (props) => {
+  const history = useHistory()
+
+  const onSubmit = (event) => {
+    event.preventDefault()
+    props.onLogin('mluukkai')
+    history.push('/')
+  }
+  
+  return (
+    <div>
+      <h2>login</h2>
+      <form onSubmit={onSubmit}>
+        <div>
+          username: <input />
+        </div>
+        <div>
+          password: <input type='password' />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  )
+}
+
+const App = () => {
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      content: 'HTML is easy',
+      important: true,
+      user: 'Matti Luukkainen'
+    },
+    {
+      id: 2,
+      content: 'Browser can execute only Javascript',
+      important: false,
+      user: 'Matti Luukkainen'
+    },
+    {
+      id: 3,
+      content: 'Most important methods of HTTP-protocol are GET and POST',
+      important: true,
+      user: 'Arto Hellas'
+    }
+  ])
+
+  const [user, setUser] = useState(null) 
+
+  const login = (user) => {
+    setUser(user)
+  }
+
+  const padding = {
+    padding: 5
+  }
+
+  const match = useRouteMatch('/notes/:id')
+  const getNoteById = () => {
+    if ( match ) {
+      console.log('-->', match.params.id)
+      return notes.find(note => note.id === Number(match.params.id))
+    }
+  }
+
+  return (
+    <div>
+      <div>
+        <Link style={padding} to="/">home</Link>
+        <Link style={padding} to="/notes">notes</Link>
+        <Link style={padding} to="/users">users</Link>
+        {user
+          ? <em>{user} logged in</em>
+          : <Link style={padding} to="/login">login</Link>
+        }
+      </div>
+
+      <Switch>
+        <Route path="/notes/:id">
+          <Note note={getNoteById()} />
+        </Route>
+        <Route path="/notes">
+          <Notes notes={notes} />
+        </Route>
+        <Route path="/users">
+          {user ? <Users /> : <Redirect to="/login" />}
+        </Route>
+        <Route path="/login">
+          <Login onLogin={login} />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+
+      <div>
+        <i>Note app, Department of Computer Science 2020</i>
+      </div>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>, 
+  document.getElementById('root')
+)
