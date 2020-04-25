f6298e6ae01146306a60a83ac3d1175e0725ee45
Peter Boone <boonepeterg@gmail.com>
2020-04-25 14:08:35 -0400

    authors and books shouldn't be const
    Can't add books or authors to the in-memory "database" if authors and books are constant.

--- library-backend.js
+++ library-backend.js
@@ -1,6 +1,6 @@
 const { ApolloServer, gql } = require('apollo-server')
 
-const authors = [
+let authors = [
   {
     name: 'Robert Martin',
     id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
@@ -31,7 +31,7 @@ const authors = [
  * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
 */
 
-const books = [
+let books = [
   {
     title: 'Clean Code',
     published: 2008,
