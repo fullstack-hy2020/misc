d90de5e212b70c6b5352beb399531882f0d556f3
jeansibelius <47578383+jeansibelius@users.noreply.github.com>
2021-11-30 13:11:50 +0900

    Fix a mistake in the English translation

--- library-backend.js
+++ library-backend.js
@@ -32,7 +32,7 @@ let authors = [
  * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
  *
  * English:
- * It might make more sense to associate a book with its author by storing the author's name in the context of the book instead of the author's id
+ * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
  * However, for simplicity, we will store the author's name in connection with the book
 */
 
