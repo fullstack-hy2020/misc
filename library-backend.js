a451efd233424a9153a15b772999f4a33f0f5762
Olivier Paspuel <107659674+olivierpas13@users.noreply.github.com>
2022-08-25 13:01:00 -0500

    Spanish traduction

--- library-backend.js
+++ library-backend.js
@@ -34,6 +34,10 @@ let authors = [
  * English:
  * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
  * However, for simplicity, we will store the author's name in connection with the book
+ *
+ * Spanish:
+ * Podría tener más sentido asociar un libro con su autor almacenando la id del autor en el contexto del libro en lugar del nombre del autor
+ * Sin embargo, por simplicidad, almacenaremos el nombre del autor en conección con el libro
 */
 
 let books = [
