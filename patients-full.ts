becbe3f7faa2cd23f1f0d6a0c965e83101ec1b18
Matti Luukkainen <mluukkai@iki.fi>
2023-02-11 09:19:52 +0200

    Update patients-full.ts

--- patients-full.ts
+++ patients-full.ts
@@ -53,7 +53,7 @@ const patients: Patient[] = [
     name: 'Hans Gruber',
     dateOfBirth: '1970-04-25',
     ssn: '250470-555L',
-    gender: Gender.Male,
+    gender: Gender.Other,
     occupation: 'Technician',
     entries: [],
   },
